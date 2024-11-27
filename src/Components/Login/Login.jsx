a391ae7b02311a12cd3f23d7017d6c8358d17670
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-28 00:07:28 +0600

    push

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,22 @@
 import { getAuth } from "firebase/auth/cordova";
 import app from "../../firebase/firebase.init";
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth/web-extension";
 
 const Login = () => {
     const auth = getAuth(app);
+    console.log(app);
     const provider = new GoogleAuthProvider();
 
 
     const handleGoogleSignIn = () => {
-        console.log('google mama is coming')
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user =result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log('error', error.massage)
+        })
     }
     return (
         <div>
--- src/__steps.js
+++ src/__steps.js
@@ -10,4 +10,6 @@
  * 9. Login.jsx: import getAuth from firebase/auth
  * 10. Create const auth = getAuth(app)
  * 11. import googleAuthProvider and create a new provider
- **/
+ * 12. use signInWithPopUp and pass auth and provider 
+ * 13. 
+ **/ 
