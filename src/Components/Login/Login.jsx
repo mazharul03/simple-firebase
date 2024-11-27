94ec088f66c228aba55965becc326bd85554263f
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-27 22:20:02 +0600

    Initialize firebase auth and set Sign in with google

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,18 @@
+import { getAuth } from "firebase/auth/cordova";
+import app from "../../firebase/firebase.init";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
 const Login = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+
+    const handleGoogleSignIn = () => {
+        console.log('google mama is coming')
+    }
     return (
         <div>
-            <button>Google login</button>
+            <button onClick={handleGoogleSignIn}>Google login</button>
         </div>
     );
 };
