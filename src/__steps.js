f517f42b3c8bbb3a6f9e4054315260a4e90b116e
Mazharul Islam <mazharul3islam0@gmail.com>
2024-11-29 00:26:09 +0600

    push

--- src/__steps.js
+++ src/__steps.js
@@ -1,16 +1,32 @@
 /**
+ *                      ----------------------
+ *                       Initial installation
+ *                      ----------------------
+ * 
  * 1. visit: consol.firebase.google.com
  * 2. create project (skip google analytics)
  * 3. Register app (create config)
  * 4. install firebase: npm install firebase
  * 5. add config file to your project
  * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
+ *                      --------------------
+ *                          Integration
+ *                      --------------------
  * 7. Visit: Go to Docs > Build > Authentication > Web > Get started 
  * 8. Export app from the firebase.config.js file: export default app
  * 9. Login.jsx: import getAuth from firebase/auth
  * 10. Create const auth = getAuth(app)
+ *                       --------------------
+ *                         Provider Setup
+ *                       --------------------
  * 11. import googleAuthProvider and create a new provider
  * 12. use signInWithPopUp and pass auth and provider 
  * 13. activate sign-in method (google, facebook, github, etc.)
  * 14. [vite]: change 127.0.0.0.1 to localhost
+ * 
+ *                      ------------------------
+ *                         More Auth provider
+ *                      ------------------------
+ * 
+ * 1. activate the auth provider (create app, provider redirect url, client id, client secret)
  **/ 
